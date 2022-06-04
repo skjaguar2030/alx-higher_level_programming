328fd13725009a10c1581e777fdf13bdefd93bf6
Aimeana100 <aimeanathole@gmail.com>
2022-06-04 17:26:46 +0200

    create a calculation file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 from add_0 import add
 a = 1
 b = 2
-print(F'{a:d} + {b:d} = {add(a,b)}')
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     from add_0 import add
