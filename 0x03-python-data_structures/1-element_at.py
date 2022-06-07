ee3d999a9c1aee632889bf2bbf086e3e957aa449
Aimeana100 <aimeanathole@gmail.com>
2022-06-07 02:28:23 +0200

    creating 1-element_at file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at( my_list, idx ):
+def element_at(my_list, idx):
     # a function that return an element in a list
     if (idx < 0 or idx > len(my_list)-1):
         return None
