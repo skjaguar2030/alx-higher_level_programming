77ed9e0939ac4637627428d7a5edec95aeb7d643
Aimeana100 <aimeanathole@gmail.com>
2022-06-07 03:15:16 +0200

    creating a  file 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a items of a matrix"""
+    for row in matrix:
+        for j in row:
+            if j != row[-1]:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end="")
+        print()
