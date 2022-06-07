2f192cdd71b273482767743fea4798fd485f164a
Aimeana100 <aimeanathole@gmail.com>
2022-06-07 03:49:23 +0200

    correct 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-
 def new_in_list(my_list, idx, element):
-    # Makes copy of a list, replce element at certain index in copy and leaving originial.
+    # Makes copy of a list, replce element
+    #  at certain index in copy and leaving originial.
     if idx < 0 or idx > len(my_list) - 1:
         return my_list[:]
     else:
