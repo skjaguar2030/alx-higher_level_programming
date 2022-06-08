e1c648a0b974952e6f668de60cd1de3f766ec1e4
Aimeana100 <aimeanathole@gmail.com>
2022-06-08 23:31:02 +0200

    create a 2-args file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-if len(sys.argv) == 1:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-elif len(sys.argv) > 1:
-    print("{:d} arguments:".format(len(sys.argv)- 1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: ".format(i+1), sys.argv[i])
-
+    from sys import argv
+if len(argv) == 1:
+    print("{:d} arguments.".format(len(argv) - 1))
+elif len(argv) > 1:
+    print("{:d} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: ".format(i+1), argv[i])
